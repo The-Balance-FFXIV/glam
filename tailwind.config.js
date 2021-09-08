a24b0064b864db9ef5e361a97f34fefb2e177d00
Stephen <stevesidwell9@gmail.com>
2021-09-07 20:55:44 -0400

    Added support for additional card type

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['layouts/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'line-divide-color': '#2a3641',
+        'link-orange': '#ff9956',
+      }
+    },
   },
   variants: {
     extend: {},
