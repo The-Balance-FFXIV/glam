664fd06ab37512017f48e15cb2f0043addfc67fd
ductapeofdoom <9399156+ductapeofdoom@users.noreply.github.com>
2021-09-11 19:25:07 -0400

    Bis and stat components (and a few small updates to other components) (#39)
    * initial branch
    * no message
    * added initial card support
    * Moved tailwind styling to main.css for easier management.
    * changed partial name from tile to card
    * fixing some gitignore changes
    * fixed theme name changes
    * removed extraneous file from gitignore
    * theme name tweak in theme.toml
    * got rid of fixed width for the card div
    * Added support for additional card type
    * Changed parameter names to be camelCase, moved most tailwind styling to main.css. Added more tailwind colors.
    * Started converting SCH layout to use the card component
    * Updated method of building the FAQ card to in theory be more extensible.
    * Added components for BiS and Stat cards
    * Tweaked the Changes card to actually pull from the front matter
    * Added link support for leveling, skills, basic, and opener.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['layouts/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'line-divide-color': '#2a3641',
+        'link-orange': '#ff9956',
+        'card-background-color' : '#323f4b',
+        'card-even-color' : '#2a3641',
+        'card-border-color' : '#3871c2',
+        'card-header-text-color' : '#7b8794',
+      }
+    },
   },
   variants: {
     extend: {},
