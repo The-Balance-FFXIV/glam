49e2b4727c955d7ad4524c1b5bb3189cd9905b52
Stephen <stevesidwell9@gmail.com>
2021-09-10 22:20:15 -0400

    Changed parameter names to be camelCase, moved most tailwind styling to main.css. Added more tailwind colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       colors: {
         'line-divide-color': '#2a3641',
         'link-orange': '#ff9956',
+        'card-background-color' : '#323f4b',
+        'card-even-color' : '#2a3641',
+        'card-border-color' : '#3871c2',
+        'card-header-text-color' : '#7b8794',
       }
     },
   },
