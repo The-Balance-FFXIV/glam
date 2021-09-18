ed22d973090d63c8158df7fe5f91e6aa58c5e072
Mike Hughes <mik3h89@gmail.com>
2021-09-17 20:06:35 -0400

    Feat/small-init-styling (#43)
    * add ignore for IDE configs
    * initial npm install
    * add role header styles, container, and bg color
    Co-authored-by: Michael Hughes <michael@tendoco.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
       colors: {
         'line-divide-color': '#2a3641',
         'link-orange': '#ff9956',
-        'card-background-color' : '#323f4b',
-        'card-even-color' : '#2a3641',
+        'card-background-color': '#44505c',
+        'card-even-color': '#3a4651',
         'card-border-color' : '#3871c2',
         'card-header-text-color' : '#7b8794',
       }
