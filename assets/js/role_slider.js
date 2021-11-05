7c3a4eab25557d7ddbacaf1532563fad103975f9
Danny Fang Yan <dannyfyan@gmail.com>
2021-11-05 14:02:20 -0700

    revert to CSS solution for checked tabs

--- assets/js/role_slider.js
+++ assets/js/role_slider.js
@@ -1,12 +1,12 @@
-const roleSliderContainer = document.getElementById("roleSlider");
-const roleSliderTabs = document.querySelectorAll(".tab");
-roleSliderTabs[0].children[0].classList.add('selected');
+// const roleSliderContainer = document.getElementById("roleSlider");
+// const roleSliderTabs = document.querySelectorAll(".tab");
+// roleSliderTabs[0].children[0].classList.add('selected');
 
-roleSliderContainer.addEventListener('mousedown', (e) => {
-    roleSliderTabs.forEach(tab => {
-        tab.children[0].classList.remove('selected');
-        if (tab.contains(e.target)) {
-            tab.children[0].classList.add('selected');
-        };
-    });
-});
+// roleSliderContainer.addEventListener('mousedown', (e) => {
+//     roleSliderTabs.forEach(tab => {
+//         tab.children[0].classList.remove('selected');
+//         if (tab.contains(e.target)) {
+//             tab.children[0].classList.add('selected');
+//         };
+//     });
+// });
