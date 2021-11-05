08d0da31abe3bc6a8db44db7cb930c48384bd4a1
Thomas Brunner <thomas.brunner@live.de>
2021-11-05 02:34:29 +0100

    refactor job single view and apply new design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,23 +5,28 @@ module.exports = {
     extend: {
       colors: {
         'line-divide-color': '#2a3641',
-        'link-orange': '#ff9956',
-        'card-background-color' : '#323f4b',
-        'card-even-color' : '#2a3641',
+        'link-orange': '#FF9900',
+        'card-light': '#19191A',
+        'card-dark': '#101111',
         'card-border-color' : '#3871c2',
         'card-header-text-color' : '#7b8794',
-        'dark-block-color' : '#1F2933'
+        'dark-block-color' : '#1F2933',
+        'tank': '#3A7AD6',
+        'healer': '#47C168',
+        'dps': '#D84D4D',
       },
       zIndex: {
           '-1': '-1',
       },
       dropShadow: {
-          'lg': '4px 4px 2px rgba(0, 0, 0, 0.2)'
+          'lg': '2px 2px 5px rgba(0, 0, 0, 0.5)'
       }
     },
   },
   variants: {
-    extend: {},
-  },
+    extend: {
+        translate: ['group-hover'],
+    },
+},
   plugins: [],
 }
