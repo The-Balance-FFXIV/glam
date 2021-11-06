34c04b928ceab9d43a925871d4aaca4a5d4fa9c6
Thomas Brunner <thomas.brunner@live.de>
2021-11-06 23:34:11 +0100

    introduce new tailwind utility class "card", "card-[color]"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   purge: ['layouts/**/*.html'],
   darkMode: false, // or 'media' or 'class'
@@ -36,5 +38,46 @@ module.exports = {
         translate: ['group-hover'],
     },
 },
-  plugins: [],
+  plugins: [
+    // Card Plugin
+    plugin(({ addUtilities, theme, e }) => {
+        const colors = theme('colors');
+
+        const cardUtilityBorderColor = Object.keys(colors).reduce((acc, key) => {
+            if (typeof colors[key] === 'string') {
+                return {
+                    ...acc,
+                    [`.card-${e(key)}`]: {
+                        '--tw-border-color': colors[key],
+                    },
+                };
+            }
+
+            const variants = Object.keys(colors[key]);
+
+            return {
+                ...acc,
+                ...variants.reduce((a, variant) => ({
+                    ...a,
+                    [`.overlay-${e(key)}-${variant}`]: {
+                        '--tw-border-color': colors[key][variant],
+                    },
+                }), {}),
+            };
+        }, {});
+
+        const cardUtilityBase = {
+            background: theme('colors.card-light'),
+            padding: '1.25rem 1.5rem',
+            borderLeft: `8px solid var(--tw-border-color, ${theme('colors.gray.600')})`,
+        };
+
+        const cardUtilities = {
+            ...cardUtilityBorderColor,
+            '.card': cardUtilityBase,
+        };
+
+        addUtilities(cardUtilities);
+    }),
+  ],
 }
