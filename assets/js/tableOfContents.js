98162b7dbf14f6c743511932a624eb5fca43a092
hintxiv <65056303+hintxiv@users.noreply.github.com>
2021-11-22 14:41:03 -0500

    nesting animations, intersection observer, qna ToC

--- assets/js/tableOfContents.js
+++ assets/js/tableOfContents.js
@@ -1,43 +1,56 @@
-function tocItem(anchor) {
-  return document.querySelector("[href=\"" + anchor + "\"]")
-}
+const anchors = document.querySelectorAll("nav#TableOfContents ul a")
+const headers = document.querySelectorAll(".markdown > h1")
+const subheaders = document.querySelectorAll(".markdown > h2")
 
-function heading(anchor) {
-  return document.querySelector("[id=" + anchor.substr(1) + "]")
-}
+function findCurrentHeader(headers) {
+  let bestHeader = null
+  let bestTop = Number.NEGATIVE_INFINITY
 
-var _anchors = null
-function anchors() {
-  if (!_anchors) {
-    _anchors = Array.from(document.querySelectorAll("#TableOfContents a"))
-        .map(e => e.getAttribute('href'))
-  }
-  return _anchors
-}
-
-function currentAnchor() {
-  var bestAnchor = null
-  var bestY = null
-  anchors().forEach(anchor => {
-    var y = heading(anchor).getBoundingClientRect().top
-    if (y >= 0 && (!bestAnchor || y < bestY)) {
-      bestAnchor = anchor
-      bestY = y
+  for (const header of headers) {
+    const top = header.getBoundingClientRect().top
+    if (top < window.innerHeight / 2 && top > bestTop) {
+      bestHeader = header
+      lowestTop = top
     }
-  })
-  return tocItem(bestAnchor)
+  }
+
+  return bestHeader
 }
 
-function update() {
-  var selected = document.getElementById("tocSelected")
-  var anchor = currentAnchor()
-  if (selected) {
-    selected.removeAttribute('id')
+const observerCallback = () => {
+  // Deactivate all anchors
+  for (const anchor of anchors) {
+    anchor.classList.remove("active")
+    const next = anchor.nextElementSibling
+
+    if (next && next.tagName === "UL") {
+      next.classList.remove("active")
+    }
   }
-  if (anchor) {
-    anchor.setAttribute('id', 'tocSelected')
+
+  const currentHeader = findCurrentHeader(headers)
+  const currentSubheader = findCurrentHeader(subheaders)
+
+  for (const header of [currentHeader, currentSubheader]) {
+    if (header != null) {
+      const anchor = document.querySelector(`a[href="#${header.id}"]`)
+      const next = anchor.nextElementSibling
+      
+      anchor.classList.add("active")
+  
+      if (next && next.tagName === "UL") {
+        next.classList.add("active")
+      }
+    }
   }
 }
 
-window.onload = update
-window.onscroll = update
+const opts = {
+	root: null,
+	rootMargin: "0px",
+}
+
+const observer = new IntersectionObserver(observerCallback, opts)
+
+headers.forEach(header => observer.observe(header))
+subheaders.forEach(header => observer.observe(header))
