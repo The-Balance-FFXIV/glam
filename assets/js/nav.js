6b1ece32e3f3888a21675407d963e4478133205b
Danny Fang Yan <dannyfyan@gmail.com>
2021-11-24 14:31:10 -0800

    PR fixes

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,16 +1,7 @@
-const navChevron = document.querySelectorAll(".chevron-down");
-const chevronUp = "/theme-assets/chevron-up.svg";
-const chevronDown = "/theme-assets/chevron-down.svg";
-
-for (let i = 0; i < navChevron.length; i++) {
-    let siblingList = navChevron[i].nextElementSibling;
-    navChevron[i].addEventListener('click', () => {
-        if (navChevron[i].children[0].src.indexOf('chevron-down') >= 0) {
-            navChevron[i].children[0].src = chevronUp
-        } else { 
-            navChevron[i].children[0].src = chevronDown
-        }
-        navChevron[i].parentNode.classList.toggle('nav-active');
+for (const chevron of document.querySelectorAll(".chevron")) {
+    let siblingList = chevron.nextElementSibling;
+    chevron.addEventListener('click', () => {
+        chevron.parentNode.classList.toggle('nav-active');
         siblingList.classList.toggle('hidden')
     });
 }
