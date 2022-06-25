2b985cb9fbe923fa19b25cadfcb92272fa2aaa7f
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2022-06-25 17:09:01 -0500

    Rendering authors, should probably refactor but eh

--- static/theme-assets/editor/job-preview.js
+++ static/theme-assets/editor/job-preview.js
@@ -1,9 +1,40 @@
 let GenericJobGuide = createClass({
   render: function () {
+    console.log(
+      "perhaps author list?",
+      this.props.entry.getIn(["data", "authors"]).map(function (author, index) {
+        console.log("individual author:", author);
+      })
+    );
     return h(
       "div",
-      { class: "job-guides-container" },
-      h("div", { class: "markdown max-w-none" }, this.props.widgetFor("body"))
+      { class: "space-y-16" },
+      h(
+        "div",
+        { class: "responsive-container" },
+        h(
+          "div",
+          { class: "job-guides-container" },
+          h(
+            "div",
+            { class: "markdown max-w-none" },
+            this.props.widgetFor("body")
+          )
+        ),
+        h("hr", {}),
+        h(
+          "div",
+          { class: "job-guides-container markdown" },
+          h("h1", {}, "Authors"),
+          h(
+            "ul",
+            {},
+            this.props.entry.getIn(["data", "authors"]).map(function (author) {
+              return h("li", {}, author);
+            })
+          )
+        )
+      )
     );
   },
 });
