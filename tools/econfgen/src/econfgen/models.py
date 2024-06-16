a31bf37084413666fca5b28e7cc31c2f94bc14a3
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2024-06-15 21:57:00 -0500

    Add PUBLISH_MODES

--- tools/econfgen/src/econfgen/models.py
+++ tools/econfgen/src/econfgen/models.py
@@ -3,11 +3,12 @@ from typing import Dict, List, Tuple, Optional, Literal
 from pydantic import BaseSettings, BaseModel
 from netlifyconfig.netlify import Backend
 
+PUBLISH_MODES = Literal['simple', 'editorial_workflow']
 
 class Environment(BaseModel):
     backend: Backend
     local_backend: Optional[bool] = None
-    publish_mode: Optional[Literal['simple', 'editorial_workflow']] = None
+    publish_mode: Optional[PUBLISH_MODES] = None
     site_url: Optional[str] = None
     show_preview_links: Optional[bool] = None
     jobs: Dict[str, List[Tuple[str, str]]]
@@ -16,7 +17,7 @@ class Environment(BaseModel):
 class OverrideableEnvironment(BaseModel):
     backend: Optional[Backend]
     local_backend: Optional[bool]
-    publish_mode: Optional[Literal['simple', 'editorial_workflow']] = None
+    publish_mode: Optional[PUBLISH_MODES] = None
     site_url: Optional[str] = None
     show_preview_links: Optional[bool] = None
     jobs: Optional[Dict[str, List[Tuple[str, str]]]]
