6aef1218412ab3d3b602010aaa27b9efa46032bc
krom <174542171+kromuluss@users.noreply.github.com>
2025-08-12 14:29:28 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,9 +30,15 @@ module.exports = {
     },
     container: {
         screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
             '3xl': '1920px',
+            '4xl': '2560px',
         }
-    }
+    },
     extend: {
       dropShadow: {
         'lg': '2px 2px 3px #000000',
