8a19dbd4b669ce36707b09e835c75cf14cd204c6
kromuluss <174542171+kromuluss@users.noreply.github.com>
2025-08-13 12:52:12 -0400

    add widescreen support and fix bis page width scaling

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,17 @@ module.exports = {
       'sans': ['Roboto', 'sans-serif'],
       'head': ['Kumbh Sans', 'sans-serif']
     },
+    container: {
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+            '3xl': '1920px',
+            '4xl': '2560px',
+        }
+    },
     extend: {
       dropShadow: {
         'lg': '2px 2px 3px #000000',
