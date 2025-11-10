7c93d23013ef106f0910243394b403327f6d12d7
XP <smurfs@smurfs.verymad.net>
2025-11-09 18:14:35 -0800

    Fix for external links

--- assets/js/externalLinks.js
+++ assets/js/externalLinks.js
@@ -1,7 +1,7 @@
 // Don't boost links to external websites
 document.body.addEventListener('htmx:configRequest', e => {
     const url = new URL(e.detail.path, window.location.origin);
-    if (url.origin !== window.location.origin) {
+    if (url.origin !== window.location.origin && e.detail.boosted) {
         // Stop HTMX from processing the event.
         e.preventDefault();   // prevent the htmx request
         // Perform normal browser navigation instead.
