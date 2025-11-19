1cf43dbf1f6cedcdb619531af389f71196085a61
XP <smurfs@smurfs.verymad.net>
2025-11-18 16:23:05 -0800

    Improve the modal dialogs

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,20 +1,12 @@
 // Open the Modal
-function openModal(clicked_id) {
-    var src = document.getElementById(clicked_id).src;
-    if (src.includes("#")) {
-      src = src.substring(0, src.indexOf( "#" ));
-    };
-    document.getElementById("modalPic").src = src;
-    document.getElementById("myModal").style.display = "block";
-    // Ensures the site footer is not shown if front of the modal
-    document.getElementById("site-footer").style.display = "hidden";
+function openModal(element) {
+    document.getElementById("modalPic").src = element.src;
+    document.getElementById("myModal").showModal();
 }
   
 // Close the Modal
 function closeModal() { 
-    // prevents flashing last modal image while new id is loading on open
+    // Prevents briefly displaying previous modal image while new image is loading.
     document.getElementById("modalPic").src = "";
-    document.getElementById("myModal").style.display = "none";
-    // See note above regarding the footer 
-    document.getElementById("site-footer").style.display = "block";
+    document.getElementById("myModal").close();
 }
